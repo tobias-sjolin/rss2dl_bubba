fcdac56222b328789c71b044deb233d69e09ef06
Tobias Sjölin <tobias@b3.localdomain>
2015-02-03 23:00:57 +0100

    Added config file and epsiode only download

--- rss2dl.py
+++ rss2dl.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python
 
 # rss2dl.py will look at an RSS feed and download programs that you want.
-# Change the progs list to have the names of the programs you want to download
 # Change the sources list to monitor the RSS feeds you want to check
 # Run this program from cron whenever you want (you can try to optimize it to
 # the times the programs will normally show up on the rss feeds, or just once
 # every 6 hours.
 
-## The Bubba user you wish to download the torrent as
-user = 'tobias'
-
-## The shows you would like to download,
-#progs = ['How I Met Your Mother -', 'The Big Bang Theory -','Community']
-
 ## Any specific filetype you don't want to download
-#blacklist = '720p .mkv'
 blacklist = ['WEB-DL','(Indi)','DD5']
 
 ## The link to our own tvtorrents rss-feed
-#sources = ['http://www.tvtorrents.com/RssServlet?digest=XXXXX']
-# Favourite torrents
 #sources = ['http://www.tvtorrents.com/mytaggedRSS?digest=XXXXX']  - TVTorrents discontinued...
 #Recent torrents
-#sources = ['http://www.tvtorrents.com/RssServlet?digest=XXXXX']
-sources = ['http://freshon.tv/rss.php?feed=dl&c[]=545&c[]=601&c[]=6221&c[]=386&c[]=577&c[]=313&c[]=552&passkey=XXXX']
+#sources = ['http://freshon.tv/rss.php?feed=dl&c[]=545&c[]=601&c[]=6221&c[]=386&c[]=577&c[]=313&c[]=55']
 #
-## Do not edit beyond this line
-#
-
-
-dir = '/home/' + user + '/torrents'
-#Change this to 1 to see all the logging
-verbose = 0
-#Number of hours to download torrents for
-timeLimitHours = 4
 
 __author__ = 'Baruch Even <baruch@ev-en.org> and Mkay'
 __version__ = '1.0.1 Bubba edition'
 
-import feedparser, re, urlparse, urllib2
+import feedparser, re, urlparse, urllib2, ConfigParser
 from os.path import basename, join, exists
 from datetime import datetime, timedelta
 
@@ -60,6 +40,28 @@ def dlfile(link):
 	f.write(torrent)
 	f.close()
 
+#Read our config file for input
+config = ConfigParser.SafeConfigParser()
+config.read("conf_rss2dl.cnf")
+#Passkey
+passkey = config.get('rss2dl', 'passkey')
+print 'Passkey: ' + passkey
+## The Bubba user you wish to download the torrent as
+user = config.get('rss2dl', 'user')
+print 'User: ' + user
+#Set the Dir to download to
+dir = '/home/' + user + '/torrents'
+print 'Dir: ' +dir
+#Change this to 1 to see all the logging
+verbose = config.getint('rss2dl', 'verbose')
+print 'Verbose: ' + str(verbose)
+#Number of hours to download torrents for
+timeLimitHours = config.getint('rss2dl', 'timeLimitHours')
+print 'timeLimitHours: ' + str(timeLimitHours)
+#Episode download only (no full Season torrents)
+episodeOnly = config.getint('rss2dl', 'episodeOnly')
+print 'EpisodeOnly: ' + str(episodeOnly)
+sources = ['http://freshon.tv/rss.php?feed=dl&c[]=545&c[]=601&c[]=6221&c[]=386&c[]=577&c[]=313&c[]=552&passkey=' + passkey]
 for src in sources:
 	rss = feedparser.parse(src)
         if verbose:
@@ -69,8 +71,7 @@ for src in sources:
             print rss.entries
 	for entry in rss.entries:
             if verbose:
-                print 'PRINT title'
-                print entry['title']
+                print 'PRINT title' + entry['title']
 	    stop = 0
             #Check if the title contains any of the blacklist words
             for blist in blacklist:
@@ -84,16 +85,15 @@ for src in sources:
                 #Convert string to datetime
                 date_object = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S')
                 if verbose:
-                    print 'PRINT now '
-                    print datetime.now()
-                    print 'PRINT pubDate '
-                    print entry['updated']                
-                    print 'PRINT pubDate fixed '
-                    print date_object
+                    print 'NOW: ' + str(datetime.now())
+                    print 'PubDate: ' + str(entry['updated'])                
+                    print 'PubDate fixed: ' + str(date_object)
                 if (datetime.now() - date_object) > timedelta(hours = timeLimitHours) and stop == 0:
-                    print entry['title'] + ' too old to download'
-                    print(datetime.now()-date_object)
-                    print date_object
+                    print entry['title'] + ' too old to download (Age: ' + str(datetime.now()-date_object) + ' since ' + str(date_object) + ')'
+                    stop = 1
+                a = re.compile("^([a-zA-Z0-9.]*)\.S([0-9]{1,2})E([0-9]{1,2}).*$")
+                if stop == 0 and episodeOnly and not a.match(entry['title']):
+                    print entry['title'] + ' Not an episode, no download'
                     stop = 1
                 #If everything is okey then download the link provided
 		if stop == 0:
